6e8a1e29687c14491672f671d57401a735e96a08
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 11:40:46 +0200

    push to heroku

--- api/routes/root.js
+++ api/routes/root.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path')
 
 router.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
  });
 
 //router.get('/', (req, res) => res.json({ message: 'API Initialized!'}) );
