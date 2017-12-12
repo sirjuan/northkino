68e666ea8d9056f4e30b71f57088762b17758191
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 10:08:26 +0200

    heroku stuff

--- api/routes/root.js
+++ api/routes/root.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const path = require('path')
 
-router.get('/', (req, res) => res.json({ message: 'API Initialized!'}) );
+router.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+//router.get('/', (req, res) => res.json({ message: 'API Initialized!'}) );
 
 module.exports = router;
