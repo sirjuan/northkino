f67d2b5c8d8b9129c7e994ebad41303b02b97dda
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 09:54:29 +0200

    heroku stuff

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api_url = process.env.NODE_ENV === 'production' ? 'https://northkino.herokuapp.com/api/xml' : 'http://localhost:3001/api/xml';
-
+//const api_url = process.env.NODE_ENV === 'production' ? 'https://northkino.herokuapp.com/api/xml' : 'http://localhost:3001/api/xml';
+const api_url = 'https://northkino.herokuapp.com/api/xml'
 export const getData = (params) => axios.get(api_url, {params}).then(result=>result.data).catch(e => console.log(e))
 
 export const current = item => {
