8b07f0d052cb0e86ebae4fb4430303da2e7a4cf8
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 17:30:17 +0200

    cleaning up

--- api/index.js
+++ api/index.js
@@ -1,25 +1,14 @@
 const app = require('express')();
 const path = require('path')
-
 const express = require('express');
 
 const { PORT} = process.env;
 
 app.use([
-
   require('./setHeaders'),
 ]);
-
-
-// Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../build')));
-
-
 app.use('/api', require('./routes/api_router'));
-
 //app.use('/*', require('./routes/root'));
-
-
 app.use(require('./errorHandlers'));
-
 app.listen(PORT, () => console.log(`API Running on ${PORT}`) );
