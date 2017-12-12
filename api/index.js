ab212eb920b31b6d3a3a66fad97e5f53dc5a8303
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 12:39:22 +0200

    push to heroku

--- api/index.js
+++ api/index.js
@@ -4,14 +4,21 @@ const path = require('path')
 const express = require('express');
 
 const { PORT} = process.env;
-app.use(express.static(path.join(__dirname, '../build', 'static')))
+
 app.use([
 
   require('./setHeaders'),
 ]);
 
-app.use('/', require('./routes/root'));
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+
 app.use('/api', require('./routes/api_router'));
+
+app.use('*', require('./routes/root'));
+
 
 app.use(require('./errorHandlers'));
 
