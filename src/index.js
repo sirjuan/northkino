77a01e2e0c6563def01e892fa04cedc56fb7e069
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 11:22:46 +0200

    push to heroku

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(process.env)
-console.log(process.env.NODE_ENV)
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
