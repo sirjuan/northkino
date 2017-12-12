6e41970c807e1498e73d1868dca751de31a8de8f
Juha Pekkarinen <sirjuha@gmail.com>
2017-12-12 11:14:23 +0200

    push to heroku

--- api/index.js
+++ api/index.js
@@ -1,28 +1,18 @@
-'use strict'
-
 const app = require('express')();
 
 const express = require('express');
 
 console.log(process.env)
 
-const { MONGO_CREDS, PORT, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_APP_SECRET } = process.env;
-
-
+const { PORT} = process.env;
 
 app.use([
-
   require('./setHeaders'),
 ]);
-
-
-
 
 app.use('/', require('./routes/root'));
 app.use('/api', require('./routes/api_router'));
 
 app.use(require('./errorHandlers'));
-
-
 
 app.listen(PORT, () => console.log(`API Running on ${PORT}`) );
--- api/routes/root.js
+++ api/routes/root.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path')
 
 router.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../..', 'build', 'index.html'));
 });
 
 //router.get('/', (req, res) => res.json({ message: 'API Initialized!'}) );
